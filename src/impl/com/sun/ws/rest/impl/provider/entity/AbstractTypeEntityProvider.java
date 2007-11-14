ce19b387766533f2fa2366e3ea6428260ff3a160
Paul Sandoz <paul.sandoz@oracle.com>
2007-11-14 13:50:46 +0000

    Removed unsued imports

--- src/impl/com/sun/ws/rest/impl/provider/entity/AbstractTypeEntityProvider.java
+++ src/impl/com/sun/ws/rest/impl/provider/entity/AbstractTypeEntityProvider.java
@@ -29,8 +29,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 
 /**
  *
