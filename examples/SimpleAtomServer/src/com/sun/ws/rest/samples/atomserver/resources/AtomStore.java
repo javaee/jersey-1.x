97821a72bc71be50e644f7054ab2511384893b03
Paul Sandoz <paul.sandoz@oracle.com>
2008-04-04 17:08:42 +0000

    Update example for changes to MessageBodyContext interface.

--- examples/SimpleAtomServer/src/com/sun/ws/rest/samples/atomserver/resources/AtomStore.java
+++ examples/SimpleAtomServer/src/com/sun/ws/rest/samples/atomserver/resources/AtomStore.java
@@ -177,14 +177,18 @@ class AtomStore {
     }    
     
     static void updateFeedDocument(MessageBodyContext bodyContext, Feed f) throws IOException {
-        MessageBodyWriter<Feed> ep = bodyContext.getMessageBodyWriter(Feed.class, atomMediaType);
+        MessageBodyWriter<Feed> ep = bodyContext.getMessageBodyWriter(
+                Feed.class, null,
+                null, atomMediaType);
         synchronized(FileStore.FS) {
             ep.writeTo(f, f.getClass(), null, null, atomMediaType, null, FileStore.FS.getFileOutputStream(AtomStore.getFeedPath()));
         }
     }
             
     static void createEntryDocument(MessageBodyContext bodyContext, String id, Entry e) throws IOException {
-        MessageBodyWriter<Entry> ep = bodyContext.getMessageBodyWriter(Entry.class, atomMediaType);
+        MessageBodyWriter<Entry> ep = bodyContext.getMessageBodyWriter(
+                Entry.class, null,
+                null, atomMediaType);
         String path = AtomStore.getEntryPath(id);
         ep.writeTo(e, e.getClass(), null, null, atomMediaType, null, FileStore.FS.getFileOutputStream(path));
     }
@@ -210,7 +214,9 @@ class AtomStore {
         selfLink.setHref(uri);
         f.getOtherLinks().add(selfLink);
         
-        MessageBodyWriter<Feed> ep = bodyContext.getMessageBodyWriter(Feed.class, atomMediaType);
+        MessageBodyWriter<Feed> ep = bodyContext.getMessageBodyWriter(
+                Feed.class, null,
+                null, atomMediaType);
         ep.writeTo(f, f.getClass(), null, null, atomMediaType, null, FileStore.FS.getFileOutputStream(AtomStore.getFeedPath()));
         
         return FileStore.FS.getFileContents(AtomStore.getFeedPath());
