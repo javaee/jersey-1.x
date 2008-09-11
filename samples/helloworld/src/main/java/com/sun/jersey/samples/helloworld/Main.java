f4a9ec57679ede28c9b19e5168a7cba521c54a42
Paul Sandoz <paul.sandoz@oracle.com>
2008-09-11 11:46:03 +0000

    No need to specify resource config class for package scanning.

--- samples/helloworld/src/main/java/com/sun/jersey/samples/helloworld/Main.java
+++ samples/helloworld/src/main/java/com/sun/jersey/samples/helloworld/Main.java
@@ -36,8 +36,8 @@ public class Main {
         final String baseUri = "http://localhost:9998/";
         final Map<String, String> initParams = new HashMap<String, String>();
 
-        initParams.put("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
-        initParams.put("com.sun.jersey.config.property.packages", "com.sun.jersey.samples.helloworld.resources");
+        initParams.put("com.sun.jersey.config.property.packages",
+                "com.sun.jersey.samples.helloworld.resources");
 
         System.out.println("Starting grizzly...");
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
