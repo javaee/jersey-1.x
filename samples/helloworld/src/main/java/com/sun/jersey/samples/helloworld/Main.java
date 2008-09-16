f36f43059254d988111d77fd1b20032698ec7a7d
Naresh <bsnaresh@java.net>
2008-09-16 17:22:52 +0000

    Fixed imports and removed commented code

--- samples/helloworld/src/main/java/com/sun/jersey/samples/helloworld/Main.java
+++ samples/helloworld/src/main/java/com/sun/jersey/samples/helloworld/Main.java
@@ -44,20 +44,6 @@ public class Main {
     }
     
     public static void main(String[] args) throws IOException {
-        /*
-        final String baseUri = "http://localhost:9998/";
-        final Map<String, String> initParams = new HashMap<String, String>();
-
-        initParams.put("com.sun.jersey.config.property.packages",
-                "com.sun.jersey.samples.helloworld.resources");
-
-        System.out.println("Starting grizzly...");
-        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-        System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nTry out %shelloworld\nHit enter to stop it...", baseUri, baseUri));
-        System.in.read();
-        threadSelector.stopEndpoint();
-        System.exit(0);
-        */
         SelectorThread threadSelector = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nTry out %shelloworld\nHit enter to stop it...",
--- samples/helloworld/src/test/java/com/sun/jersey/samples/helloworld/MainTest.java
+++ samples/helloworld/src/test/java/com/sun/jersey/samples/helloworld/MainTest.java
@@ -40,7 +40,6 @@ package com.sun.jersey.samples.helloworld;
 import com.sun.grizzly.http.SelectorThread;
 import com.sun.jersey.api.MediaTypes;
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import junit.framework.TestCase;
 
