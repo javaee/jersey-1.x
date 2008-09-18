ca64e40f04ae84951311b93237f1b573e594e366
Jakub Podlesak <jakub.podlesak@oracle.com>
2008-09-18 12:25:38 +0000

    simplified method for ensuring valid xml names

--- jersey-core/src/main/java/com/sun/jersey/impl/provider/entity/AbstractListElementProvider.java
+++ jersey-core/src/main/java/com/sun/jersey/impl/provider/entity/AbstractListElementProvider.java
@@ -212,18 +212,11 @@ public abstract class AbstractListElementProvider extends AbstractJAXBProvider<C
     
     private final Inflector inflector = Inflector.getInstance();
 
-    private final String ensureValidXmlName(String name) {
-        if (name == null) {
-            return null;
-        }
-        if (name.contains("$")) {
-            return name.replace("$", "");
-        } else {
-            return name;
-        }
+    private final String convertToXmlName(final String name) {
+        return name.replace("$", "_");
     }
 
     protected final String getRootElementName(Class<?> elementType) {
-        return ensureValidXmlName(inflector.pluralize(inflector.demodulize(elementType.getName())));
+        return convertToXmlName(inflector.pluralize(inflector.demodulize(elementType.getName())));
     }    
 }
