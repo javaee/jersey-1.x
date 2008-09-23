5ece7c1ce457757def7a4525b7c90dca7ee618d3
Paul Sandoz <paul.sandoz@oracle.com>
2008-09-23 14:24:10 +0000

    Upgrade to scala 2.7.2.rc2. Unfortunately a bug in the scala compiler means that a unit test cannot be created.

--- samples/mandel/src/main/scala/com/sun/jersey/samples/mandel/MandelServer.scala
+++ samples/mandel/src/main/scala/com/sun/jersey/samples/mandel/MandelServer.scala
@@ -1,34 +1,58 @@
-package com.sun.jersey.samples.mandel
+
+import java.net.URI;
+import java.util.HashMap;
+import javax.ws.rs.core.UriBuilder;
+import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+
+package com.sun.jersey.samples.mandel {
 
 object MandelServer {
-    import com.sun.net.httpserver.HttpServer
-    import com.sun.net.httpserver.HttpHandler
-    import com.sun.jersey.api.container.httpserver.HttpServerFactory
-    import com.sun.jersey.api.container.ContainerFactory
-    import com.sun.jersey.api.core.PackagesResourceConfig
+    private def getPort(defaultPort : int) = {
+        val port = System.getenv("JERSEY_HTTP_PORT");
 
-    def run() {
-        val pkgs: Array[String] = new Array[String](1);
-        pkgs(0) = "com.sun.jersey.samples.mandel"
+        if (null != port)
+            try  {
+            Integer.parseInt(port);
+            } catch {
+                case ex: NumberFormatException => defaultPort;
+            }
+        else
+            defaultPort;
+    }
 
-        val handler = ContainerFactory.createContainer(classOf[HttpHandler], new PackagesResourceConfig(pkgs));
-        val server = HttpServerFactory.create("http://localhost:9998/mandelbrot", handler);
-        server.start();
+    private def getBaseURI() = {
+        UriBuilder.fromUri("http://localhost/").port(getPort(9998)).
+            path("mandelbrot").build();
+    }
+
+    val BASE_URI = getBaseURI();
+
+    def startServer() = {
+        val initParams = new HashMap[String, String]();
+        initParams.put("com.sun.jersey.config.property.packages",
+                "com.sun.jersey.samples.mandel");
+
+        System.out.println("Starting grizzly...");
+        GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+    }
+
+    def main(args: Array[String]) {
+        val selectorThread = startServer();
 
         println("Server running");
-        println("Visit: http://localhost:9998/mandelbrot/(-2.2,-1.2),(0.8,1.2)");
+        println("Visit: " + BASE_URI + "/(-2.2,-1.2),(0.8,1.2)");
         println("The query parameter 'limit' specifies the limit on number of iterations");
         println("to determine if a point on the complex plain belongs to the mandelbrot set");
         println("The query parameter 'imageSize' specifies the maximum size of the image");
         println("in either the horizontal or virtical direction");
         println("Hit return to stop...");
         System.in.read();
-        println("Stopping server");   
-        server.stop(0);
+        println("Stopping server");
+        selectorThread.stopEndpoint();
         println("Server stopped");
+        System.exit(0);
     }
+}
 
-    def main(args: Array[String]) {
-        run()
-    }
 }
