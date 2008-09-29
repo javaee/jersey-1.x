2d58645d6d75fd03017314d42a43cb25a9358a1f
Naresh <bsnaresh@java.net>
2008-09-29 11:11:38 +0000

    Did a split of the code to logical units. Ex: one for starting the server, etc.

--- samples/entity-provider/src/main/java/com/sun/jersey/samples/entityprovider/Main.java
+++ samples/entity-provider/src/main/java/com/sun/jersey/samples/entityprovider/Main.java
@@ -38,25 +38,51 @@
 package com.sun.jersey.samples.entityprovider;
 
 import com.sun.grizzly.http.SelectorThread;
-import com.sun.jersey.api.container.grizzly.*;
-import com.sun.jersey.impl.container.grizzly.*;
-import java.util.*;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.UriBuilder;
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        final String baseUri = "http://localhost:9998/";
+
+    private static int getPort(int defaultPort) {
+        String port = System.getenv("JERSEY_HTTP_PORT");
+        if (null != port) {
+            try {
+                return Integer.parseInt(port);
+            } catch (NumberFormatException e) {
+            }
+        }
+        return defaultPort;
+    }
+
+    private static URI getBaseURI() {
+        return UriBuilder.fromUri("http://localhost/").port(getPort(9998)).build();
+    }
+
+    public static final URI BASE_URI = getBaseURI();
+
+    protected static SelectorThread startServer() throws IOException {
         final Map<String, String> initParams = new HashMap<String, String>();
 
-        initParams.put("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
-        initParams.put("com.sun.jersey.config.property.packages", "com.sun.jersey.samples.entityprovider");
+        initParams.put("com.sun.jersey.config.property.resourceConfigClass",
+                "com.sun.jersey.api.core.PackagesResourceConfig");
+        initParams.put("com.sun.jersey.config.property.packages",
+                "com.sun.jersey.samples.entityprovider");
 
         System.out.println("Starting grizzly...");
-        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-        System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nHit enter to stop it...", baseUri));
+        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+        return threadSelector;
+    }
+
+    
+    public static void main(String[] args) throws IOException {
+        SelectorThread threadSelector = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
-        threadSelector.stop();
+        threadSelector.stopEndpoint();
         System.exit(0);
     }    
 }
