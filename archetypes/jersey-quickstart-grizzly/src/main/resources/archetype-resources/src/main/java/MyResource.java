60de460af0bc4189a48b47d90b3c970518316b6f
Jakub Podlesak <jakub.podlesak@oracle.com>
2008-10-21 13:50:21 +0000

    Renaming resource in the grizzly archetype to be more general

--- archetypes/jersey-quickstart-grizzly/src/main/resources/archetype-resources/src/main/java/MyResource.java
+++ archetypes/jersey-quickstart-grizzly/src/main/resources/archetype-resources/src/main/java/MyResource.java
@@ -5,17 +5,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-// The Java class will be hosted at the URI path "/helloworld"
-@Path("/helloworld")
-public class HelloWorldResource {
+// The Java class will be hosted at the URI path "/myresource"
+@Path("/myresource")
+public class MyResource {
+
+    // TODO: update the class to suit your needs
     
     // The Java method will process HTTP GET requests
     @GET 
     // The Java method will produce content identified by the MIME Media
     // type "text/plain"
     @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    public String getIt() {
+        return "Got it!";
     }
 }
