6ce6f623d05b7f5e73fb8fc9e1ae271c83cc3d66
Jakub Podlesak <jakub.podlesak@oracle.com>
2008-10-22 16:27:30 +0000

    fixing WAR quickstart archetype, TODO: add glassfish plugin

--- archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/java/MyResource.java
+++ archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/java/MyResource.java
@@ -6,16 +6,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 // The Java class will be hosted at the URI path "/helloworld"
-@Path("/helloworld")
-public class HelloWorldResource {
+@Path("/myresource")
+public class MyResource {
     
     // The Java method will process HTTP GET requests
     @GET 
     // The Java method will produce content identified by the MIME Media
     // type "text/plain"
     @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    public String getIt() {
+        return "Hi there!";
     }
 }
--- archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/webapp/index.jsp
+++ archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Jersey RESTful Web Application!</h2>
-<p><a href="webresources/helloworld">helloworld resource</a>
+<p><a href="webresources/myresource">Jersey resource</a>
 <p>Visit <a href="https://jersey.dev.java.net">Jersey project</a> page
 for more information on Jersey!
 </body>
