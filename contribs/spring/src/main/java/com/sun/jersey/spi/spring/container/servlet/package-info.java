75cf903c5fcaf220958d8ccb13f14d7447b9b9de
Paul Sandoz <paul.sandoz@oracle.com>
2008-11-10 13:12:45 +0000

    Improve JavaDoc and generate JavaDoc jar.

--- contribs/spring/src/main/java/com/sun/jersey/spi/spring/container/servlet/package-info.java
+++ contribs/spring/src/main/java/com/sun/jersey/spi/spring/container/servlet/package-info.java
@@ -24,8 +24,58 @@
  * <p>
  * Spring support is enabled by referencing the the servlet
  * {@link com.sun.jersey.spi.spring.container.servlet.SpringServlet} in the
- * web.xml.
- * <p>
+ * web.xml. For example:
+ * <blockquote><pre>
+ *   &lt;web-app&gt;
+ *     &lt;context-param&gt;
+ *       &lt;param-name&gt;contextConfigLocation&lt;/param-name&gt;
+ *       &lt;param-value&gt;classpath:applicationContext.xml&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     &lt;listener&gt;
+ *       &lt;listener-class&gt;org.springframework.web.context.ContextLoaderListener&lt;/listener-class&gt;
+ *     &lt;/listener&gt;
+ *     &lt;servlet&gt;
+ *       &lt;servlet-name&gt;Jersey Spring Web Application&lt;/servlet-name&gt;
+ *       &lt;servlet-class&gt;com.sun.jersey.spi.spring.container.servlet.SpringServlet&lt;/servlet-class&gt;
+ *     &lt;/servlet&gt;
+ *     &lt;servlet-mapping&gt;
+ *       &lt;servlet-name&gt;Jersey Spring Web Application&lt;/servlet-name&gt;
+ *       &lt;url-pattern&gt;/*&lt;/url-pattern&gt;
+ *     &lt;/servlet-mapping&gt;
+ *   &lt;/web-app&gt;
+ * </blockquote></pre>
+ * Any root resource classes or provider classes declared by Spring (in the XML
+ * configuration or by auto-wiring) will be automatically registered. It is
+ * possible to intermix Spring and non-Spring registration of classes by
+ * additionally using the normal Jersey-based registration mechanisms. For example,
+ * in the following web.xml:
+ * <blockquote><pre>
+ *   &lt;web-app&gt;
+ *     &lt;context-param&gt;
+ *       &lt;param-name&gt;contextConfigLocation&lt;/param-name&gt;
+ *       &lt;param-value&gt;classpath:applicationContext.xml&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     &lt;listener&gt;
+ *       &lt;listener-class&gt;org.springframework.web.context.ContextLoaderListener&lt;/listener-class&gt;
+ *     &lt;/listener&gt;
+ *     &lt;servlet&gt;
+ *       &lt;servlet-name&gt;Jersey Spring Web Application&lt;/servlet-name&gt;
+ *       &lt;servlet-class&gt;com.sun.jersey.spi.spring.container.servlet.SpringServlet&lt;/servlet-class&gt;
+ *       &lt;init-param&gt;
+ *           &lt;param-name&gt;com.sun.jersey.config.property.packages&lt;/param-name&gt;
+ *           &lt;param-value&gt;managed&lt;/param-value&gt;
+ *       &lt;/init-param>
+ *     &lt;/servlet&gt;
+ *     &lt;servlet-mapping&gt;
+ *       &lt;servlet-name&gt;Jersey Spring Web Application&lt;/servlet-name&gt;
+ *       &lt;url-pattern&gt;/*&lt;/url-pattern&gt;
+ *     &lt;/servlet-mapping&gt;
+ *   &lt;/web-app&gt;
+ * </blockquote></pre>
+ * Spring-based classes will be registered and any root resource and provider
+ * classes in the package <code>managed</code> will also be registered. A class
+ * will only be registered at most once so it does not matter if there are Spring
+ * managed classes present in the <code>managed</code> package.
  * Components managed by Spring will not be managed by Jersey and the 
  * Jersey-based life-cycle annotations on a resource class will be ignored.
  */
