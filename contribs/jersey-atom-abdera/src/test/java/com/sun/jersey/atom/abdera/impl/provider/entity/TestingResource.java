13f70e24b1b4f52ab5b84fd700f9788924118570
craigmcc <craigmcc@java.net>
2009-01-09 03:52:05 +0000

    Add some logic that determines which of the media types listed in @Produces we are actually going to be sending.

--- contribs/jersey-atom-abdera/src/test/java/com/sun/jersey/atom/abdera/impl/provider/entity/TestingResource.java
+++ contribs/jersey-atom-abdera/src/test/java/com/sun/jersey/atom/abdera/impl/provider/entity/TestingResource.java
@@ -38,12 +38,18 @@
 package com.sun.jersey.atom.abdera.impl.provider.entity;
 
 import com.sun.jersey.atom.abdera.ContentHelper;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Variant;
 import org.apache.abdera.model.Entry;
 
 /**
@@ -55,10 +61,14 @@ public class TestingResource {
     @Context
     ContentHelper contentHelper;
 
+    @Context
+    Request request;
+
     @GET
     @Path("categories")
     @Produces({"application/atomcat+xml", "application/xml", "text/xml", "application/atomcat+json", "application/json"})
     public Response getCategories() {
+        dumpSelectedMediaType("getCategories");
         return Response.ok(TestingFactory.createCategories()).build();
     }
 
@@ -66,6 +76,7 @@ public class TestingResource {
     @Path("content")
     @Produces("application/xml")
     public Response getContent() {
+        dumpSelectedMediaType("getContent");
         Entry entry = TestingFactory.createEntry();
         ContentBean bean = new ContentBean("foo value", "bar value");
         contentHelper.setContentEntity(entry, MediaType.APPLICATION_XML_TYPE, bean);
@@ -76,6 +87,7 @@ public class TestingResource {
     @Path("entry")
     @Produces({"application/atom", "application/atom+xml", "application/xml", "text/xml", "application/atom+json", "application/json"})
     public Response getEntry() {
+        dumpSelectedMediaType("getEntry");
         return Response.ok(TestingFactory.createEntry()).build();
     }
 
@@ -83,6 +95,7 @@ public class TestingResource {
     @Path("feed")
     @Produces({"application/atom", "application/atom+xml", "application/xml", "text/xml", "application/atom+json", "application/json"})
     public Response getFeed() {
+        dumpSelectedMediaType("getFeed");
         return Response.ok(TestingFactory.createFeed()).build();
     }
 
@@ -90,7 +103,55 @@ public class TestingResource {
     @Path("service")
     @Produces({"application/atomsvc+xml", "application/xml", "text/xml", "application/atomsvc+json", "application/json"})
     public Response getService() {
+        dumpSelectedMediaType("getService");
         return Response.ok(TestingFactory.createService()).build();
+    }
+
+    private void dumpSelectedMediaType(String methodName) {
+        System.out.println("Method " + methodName + " selected media type " + calculateSelectedMediaType(methodName));
+    }
+
+    /**
+     * <p>Calculate and return the media type that we should be producing
+     * on this call, following the same algorithm that JAX-RS does.</p>
+     *
+     * @param methodName Name of the method (in this class) that was called
+     *
+     * @exception IllegalArgumentException if we cannot identify the appropriate
+     *  @Produces annotation for this method name
+     */
+    private MediaType calculateSelectedMediaType(String methodName) {
+        // Reflect to get the Method definition for this methodName
+        // The following logic assumes none of the method names are overloaded,
+        // which is true for this class but might not be in general
+        Method[] methods = this.getClass().getDeclaredMethods();
+        Method method = null;
+        for (int i = 0; i < methods.length; i++) {
+            if (methodName.equals(methods[i].getName())) {
+                method = methods[i];
+                break;
+            }
+        }
+        if (method == null) {
+            throw new IllegalArgumentException("Cannot find Method instance for method " + methodName);
+        }
+        // Look for the @Produces annotation on this method
+        Annotation annotation = method.getAnnotation(Produces.class);
+        if (annotation == null) {
+            throw new IllegalArgumentException("Method " + methodName + " does not have an @Produces annotation");
+        }
+        // Build a list of Variants based on the @Produces annotation
+        List<Variant> variants = new ArrayList<Variant>();
+        String[] items = ((Produces) annotation).value();
+        for (String item : items) {
+            String[] parts = item.split("/");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("Media type " + item + " on @Produces list for method " + methodName + " is malformed");
+            }
+            variants.add(new Variant(new MediaType(parts[0], parts[1]), null, null));
+        }
+        // Return the media type from the variant that was selected
+        return request.selectVariant(variants).getMediaType();
     }
 
 }
