21b237cecc8d1dcfa20d189a3db597f9572f26f4
Martin Matula <martin.matula@oracle.com>
2009-04-02 16:03:46 +0000

    Minor cleanup of the archetype resource.

--- archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/java/MyResource.java
+++ archetypes/jersey-quickstart-webapp/src/main/resources/archetype-resources/src/main/java/MyResource.java
@@ -5,14 +5,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-// The Java class will be hosted at the URI path "/helloworld"
+/** Example resource class hosted at the URI path "/myresource"
+ */
 @Path("/myresource")
 public class MyResource {
     
-    // The Java method will process HTTP GET requests
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
     @GET 
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
     @Produces("text/plain")
     public String getIt() {
         return "Hi there!";
