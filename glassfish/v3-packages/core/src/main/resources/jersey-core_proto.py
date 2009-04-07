a8e5760f7621a9f10e5d5d97ef506ce0e76f469c
Jakub Podlesak <jakub.podlesak@oracle.com>
2009-04-07 09:25:04 +0000

    gfv3 ips core package update: added jettison and jackson libs, updated license, todo: add password=GlassFish osgi param

--- glassfish/v3-packages/core/src/main/resources/jersey-core_proto.py
+++ glassfish/v3-packages/core/src/main/resources/jersey-core_proto.py
@@ -1,21 +1,19 @@
 pkg = {
-    "name"          : "jersey-core",
+    "name"          : "jersey",
     "version"       : "1.0.3,0-0.1",
     "attributes"    : { "pkg.summary" : "Jersey Core, RESTful Web services for GlassFish",
                         "pkg.description" : 
-"Jersey is the open source (under dual CDDL+GPL license)\
+"Jersey core runtime libraries including some 3rd party dependencies. \
+ Jersey is the open source (under dual CDDL+GPL license)\
  JAX-RS (JSR 311) Reference Implementation for building RESTful Web services. \
  But, it is also more than the Reference Implementation. \
  Jersey provides additional APIs and extension points (SPIs) \
- so that developers may extend Jersey to suite their needs. \
- This package contains Jersey core runtime libraries",
+ so that developers may extend Jersey to suite their needs.",
                      "info.classification" : "Web Services"  },
     "dirtrees" : [ "glassfish"],
     "depends" : { 
-                  "pkg:/asm@3.1" : {"type" : "require" }
+                  "pkg:/glassfish-nucleus@3.0" : {"type" : "require" }
                   ,"pkg:/metro@1.4" : {"type" : "require" }
-                  ,"pkg:/jettison@1.0.1" : {"type" : "require" }
-                  ,"pkg:/jackson@0.9.4" : {"type" : "require" }
                 },
     "licenses" : { "LICENSE.txt" : { "license" : "CDDL+GPL" }}
 }
