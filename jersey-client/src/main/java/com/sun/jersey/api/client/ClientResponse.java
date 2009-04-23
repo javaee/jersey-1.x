d8c53d186c667228bf76ee79bed2411e46843fb0
Paul Sandoz <paul.sandoz@oracle.com>
2009-04-23 11:48:48 +0000

    - Fixed issue 151   https://jersey.dev.java.net/issues/show_bug.cgi?id=151   Added the following to ClientResponse:   - enum Status, containing all known HTTP status codes   - ClientResponse.getClientResponseStatus() that returns an instance of Status

--- jersey-client/src/main/java/com/sun/jersey/api/client/ClientResponse.java
+++ jersey-client/src/main/java/com/sun/jersey/api/client/ClientResponse.java
@@ -61,6 +61,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
+import javax.ws.rs.core.Response.StatusType.Family;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.RuntimeDelegate;
 import javax.ws.rs.ext.RuntimeDelegate.HeaderDelegate;
@@ -71,6 +73,239 @@ import javax.ws.rs.ext.RuntimeDelegate.HeaderDelegate;
  * @author Paul.Sandoz@Sun.Com
  */
 public class ClientResponse {
+
+    /**
+     * Status codes defined by HTTP, see
+     * {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10">HTTP/1.1 documentation</a>}.
+     * Additional status codes can be added by applications by creating an implementation of {@link StatusType}.
+     */
+    public enum Status implements StatusType {
+        /**
+         * 200 OK, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.1">HTTP/1.1 documentation</a>}.
+         */
+        OK(200, "OK"),
+        /**
+         * 201 Created, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.2">HTTP/1.1 documentation</a>}.
+         */
+        CREATED(201, "Created"),
+        /**
+         * 202 Accepted, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.3">HTTP/1.1 documentation</a>}.
+         */
+        ACCEPTED(202, "Accepted"),
+        /**
+         * 202 Accepted, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.4">HTTP/1.1 documentation</a>}.
+         */
+        NON_AUTHORITIVE_INFORMATION(203, "Non-Authoritative Information"),
+        /**
+         * 204 No Content, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.5">HTTP/1.1 documentation</a>}.
+         */
+        NO_CONTENT(204, "No Content"),
+        /**
+         * 205 Reset Content, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.6">HTTP/1.1 documentation</a>}.
+         */
+        RESET_CONTENT(205, "Reset Content"),
+        /**
+         * 206 Reset Content, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.7">HTTP/1.1 documentation</a>}.
+         */
+        PARTIAL_CONTENT(206, "Partial Content"),
+
+
+        /**
+         * 301 Moved Permantely, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.2">HTTP/1.1 documentation</a>}.
+         */
+        MOVED_PERMANENTLY(301, "Moved Permanently"),
+        /**
+         * 302 Found, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.3">HTTP/1.1 documentation</a>}.
+         */
+        FOUND(302, "Found"),
+        /**
+         * 303 See Other, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.4">HTTP/1.1 documentation</a>}.
+         */
+        SEE_OTHER(303, "See Other"),
+        /**
+         * 304 Not Modified, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5">HTTP/1.1 documentation</a>}.
+         */
+        NOT_MODIFIED(304, "Not Modified"),
+        /**
+         * 305 Use Proxy, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.6">HTTP/1.1 documentation</a>}.
+         */
+        USE_PROXY(305, "Use Proxy"),
+        /**
+         * 307 Temporary Redirect, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.8">HTTP/1.1 documentation</a>}.
+         */
+        TEMPORARY_REDIRECT(307, "Temporary Redirect"),
+
+        
+        /**
+         * 400 Bad Request, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.1">HTTP/1.1 documentation</a>}.
+         */
+        BAD_REQUEST(400, "Bad Request"),
+        /**
+         * 401 Unauthorized, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.2">HTTP/1.1 documentation</a>}.
+         */
+        UNAUTHORIZED(401, "Unauthorized"),
+        /**
+         * 402 Payment Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.3">HTTP/1.1 documentation</a>}.
+         */
+        PAYMENT_REQUIRED(402, "Payment Required"),
+        /**
+         * 403 Forbidden, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.4">HTTP/1.1 documentation</a>}.
+         */
+        FORBIDDEN(403, "Forbidden"),
+        /**
+         * 404 Not Found, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.5">HTTP/1.1 documentation</a>}.
+         */
+        NOT_FOUND(404, "Not Found"),
+        /**
+         * 405 Method Not Allowed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.6">HTTP/1.1 documentation</a>}.
+         */
+        METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+        /**
+         * 406 Not Acceptable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.7">HTTP/1.1 documentation</a>}.
+         */
+        NOT_ACCEPTABLE(406, "Not Acceptable"),
+        /**
+         * 407 Proxy Authentication Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.8">HTTP/1.1 documentation</a>}.
+         */
+        PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
+        /**
+         * 408 Request Timeout, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.9">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_TIMEOUT(408, "Request Timeout"),
+        /**
+         * 409 Conflict, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.10">HTTP/1.1 documentation</a>}.
+         */
+        CONFLICT(409, "Conflict"),
+        /**
+         * 410 Gone, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.11">HTTP/1.1 documentation</a>}.
+         */
+        GONE(410, "Gone"),
+        /**
+         * 411 Length Required, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.12">HTTP/1.1 documentation</a>}.
+         */
+        LENGTH_REQUIRED(411, "Length Required"),
+        /**
+         * 412 Precondition Failed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.13">HTTP/1.1 documentation</a>}.
+         */
+        PRECONDITION_FAILED(412, "Precondition Failed"),
+        /**
+         * 413 Request Entity Too Large, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.14">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_ENTITY_TOO_LARGE(413, "Request Entity Too Large"),
+        /**
+         * 414 Request-URI Too Long, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.15">HTTP/1.1 documentation</a>}.
+         */
+        REQUEST_URI_TOO_LONG(414, "Request-URI Too Long"),
+        /**
+         * 415 Unsupported Media Type, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.16">HTTP/1.1 documentation</a>}.
+         */
+        UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
+        /**
+         * 416 Requested Range Not Satisfiable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.17">HTTP/1.1 documentation</a>}.
+         */
+        REQUESTED_RANGE_NOT_SATIFIABLE(416, "Requested Range Not Satisfiable"),
+        /**
+         * 417 Expectation Failed, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.18">HTTP/1.1 documentation</a>}.
+         */
+        EXPECTATION_FAILED(417, "Expectation Failed"),
+
+
+        /**
+         * 500 Internal Server Error, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.1">HTTP/1.1 documentation</a>}.
+         */
+        INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+        /**
+         * 501 Not Implemented, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.2">HTTP/1.1 documentation</a>}.
+         */
+        NOT_IMPLEMENTED(501, "Not Implemented"),
+        /**
+         * 502 Bad Gateway, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.3">HTTP/1.1 documentation</a>}.
+         */
+        BAD_GATEWAY(502, "Bad Gateway"),
+        /**
+         * 503 Service Unavailable, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.4">HTTP/1.1 documentation</a>}.
+         */
+        SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+        /**
+         * 504 Gateway Timeout, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.5">HTTP/1.1 documentation</a>}.
+         */
+        GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+        /**
+         * 505 HTTP Version Not Supported, see {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.5.6">HTTP/1.1 documentation</a>}.
+         */
+        HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
+
+
+        private final int code;
+        private final String reason;
+        private Family family;
+
+        Status(final int statusCode, final String reasonPhrase) {
+            this.code = statusCode;
+            this.reason = reasonPhrase;
+            switch(code/100) {
+                case 1: this.family = Family.INFORMATIONAL; break;
+                case 2: this.family = Family.SUCCESSFUL; break;
+                case 3: this.family = Family.REDIRECTION; break;
+                case 4: this.family = Family.CLIENT_ERROR; break;
+                case 5: this.family = Family.SERVER_ERROR; break;
+                default: this.family = Family.OTHER; break;
+            }
+        }
+
+        /**
+         * Get the class of status code.
+         *
+         * @return the class of status code.
+         */
+        public Family getFamily() {
+            return family;
+        }
+
+        /**
+         * Get the associated status code.
+         *
+         * @return the status code
+         */
+        public int getStatusCode() {
+            return code;
+        }
+
+        /**
+         * Get the reason phrase.
+         *
+         * @return the reason phrase.
+         */
+        public String getReasonPhrase() {
+            return toString();
+        }
+
+        /**
+         * Get the reason phrase.
+         *
+         * @return the reason phrase.
+         */
+        @Override
+        public String toString() {
+            return reason;
+        }
+
+        /**
+         * Convert a numerical status code into the corresponding Status.
+         *
+         * @param statusCode the numerical status code.
+         * @return the matching Status or null is no matching Status is defined.
+         */
+        public static Status fromStatusCode(final int statusCode) {
+            for (Status s : Status.values()) {
+                if (s.code == statusCode) {
+                    return s;
+                }
+            }
+            return null;
+        }
+    }
+
     private static final Annotation[] EMPTY_ANNOTATIONS = new Annotation[0];
     
     protected static final HeaderDelegate<EntityTag> entityTagDelegate = 
@@ -132,13 +367,36 @@ public class ClientResponse {
     }
 
     /**
+     * Set the status code.
+     *
+     * @param status the status code.
+     */
+    public void setStatus(Response.StatusType status) {
+        setStatus(status.getStatusCode());
+    }
+
+    /**
+     * Get the status code.
+     *
+     * @return the status code, or null if the underlying status code was set
+     *         using the method {@link #setStatus(int)} and there is no
+     *         mapping between the the integer value and the Response.Status
+     *         enumeration value.
+     */
+    public Status getClientResponseStatus() {
+        return Status.fromStatusCode(status);
+    }
+
+    /**
      * Get the status code.
      * 
      * @return the status code, or null if the underlying status code was set
      *         using the method {@link #setStatus(int)} and there is no
      *         mapping between the the integer value and the Response.Status
      *         enumeration value.
+     * @deprecated use {@link #getClientResponseStatus()
      */
+    @Deprecated
     public Response.Status getResponseStatus() {
         return Response.Status.fromStatusCode(status);
     }
@@ -147,9 +405,11 @@ public class ClientResponse {
      * Set the status code.
      * 
      * @param status the status code.
+     * @deprecated see {@link #setStatus(javax.ws.rs.core.Response.StatusType) 
      */
-    public void setResponseStatus(Response.Status status) {
-        setStatus(status.getStatusCode());
+    @Deprecated
+    public void setResponseStatus(Response.StatusType status) {
+        setStatus(status);
     }
     
     /**
