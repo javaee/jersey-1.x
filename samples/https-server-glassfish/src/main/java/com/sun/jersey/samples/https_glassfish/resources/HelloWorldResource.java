3948741f01556707c5d1b239e395ec83ce12bef2
Pavel Bucek <pavel.bucek@oracle.com>
2009-04-25 15:12:37 +0000

    adding https-server-glassfish sample

---
+++ samples/https-server-glassfish/src/main/java/com/sun/jersey/samples/https_glassfish/resources/HelloWorldResource.java
@@ -0,0 +1,53 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007 Sun Microsystems, Inc. All rights reserved. 
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License("CDDL") (the "License").  You may not use this file
+ * except in compliance with the License. 
+ * 
+ * You can obtain a copy of the License at:
+ *     https://jersey.dev.java.net/license.txt
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice in each
+ * file and include the License file at:
+ *     https://jersey.dev.java.net/license.txt
+ * If applicable, add the following below this CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+package com.sun.jersey.samples.https_glassfish.resources;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+@RolesAllowed("myRole")
+public class HelloWorldResource {
+
+    // The Java method will process HTTP GET requests
+    @GET 
+    // The Java method will produce content identified by the MIME Media
+    // type "text/plain"
+    @Produces("text/plain")
+    public Response getClichedMessage(@Context HttpServletRequest request) {
+        // Return some cliched textual content
+
+            return Response.
+                    status(Response.Status.OK).
+                    type("text/plain").
+                    entity("Sending \"Hello World\" to user \"" + request.getUserPrincipal().getName() + "\"").
+                    build();
+
+    }
+}
