51c4153b4c94f60b51a96acfc6c5d3fdee90919d
James Strachan <james.strachan@gmail.com>
2009-04-27 11:58:57 +0000

    added a basic scala module so that you can easily render Scala markup as HTML/XML/XHTML as per this issue https://jersey.dev.java.net/issues/show_bug.cgi?id=281 and added an example to the scala sample web app

---
+++ contribs/jersey-scala/src/main/scala/com/sun/jersey/scala/NodeWriter.scala
@@ -0,0 +1,32 @@
+package com.sun.jersey.scala
+
+
+import _root_.java.io.OutputStream
+import _root_.java.lang.annotation.Annotation
+import _root_.java.lang.{String, Class}
+
+import javax.ws.rs.core.{MultivaluedMap, MediaType}
+import javax.ws.rs.ext.{MessageBodyWriter, Provider}
+import _root_.java.lang.reflect.Type
+
+import scala.xml.NodeSeq
+
+/**
+ * Converts a Scala {@link NodeSeq} to a String for rendering nodes as HTML, XML, XHTML etc
+ *
+ * @version $Revision: 1.1 $
+ */
+@Provider
+class NodeWriter extends MessageBodyWriter[NodeSeq] {
+
+  def isWriteable(aClass: Class[_], aType: Type, annotations: Array[Annotation], mediaType: MediaType) = {
+    classOf[NodeSeq].isAssignableFrom(aClass)
+  }
+
+  def getSize(nodes: NodeSeq, aClass: Class[_], aType: Type, annotations: Array[Annotation], mediaType: MediaType) = -1L
+
+  def writeTo(nodes: NodeSeq, aClass: Class[_], aType: Type, annotations: Array[Annotation], mediaType: MediaType, stringObjectMultivaluedMap: MultivaluedMap[String, Object], outputStream: OutputStream) : Unit = {
+    var answer = nodes.toString();
+    outputStream.write(answer.getBytes());
+  }
+}
