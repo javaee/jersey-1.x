ed5b5974e8d753fd1d00d7128964b142b1ec54cf
Jakub Podlesak <jakub.podlesak@oracle.com>
2009-07-13 14:50:22 +0000

    updated jersey version for the next snapshots

--- glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
+++ glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
@@ -1,6 +1,6 @@
 pkg = {
     "name"          : "jersey-docs-and-examples",
-    "version"       : "1.1.1,0-0.1",
+    "version"       : "1.1.2,0-0.1",
     "attributes"    : { "pkg.summary" : "Jersey Examples And Documentation, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "This package contains Jersey API javadocs and examples. Once installed, \
@@ -13,7 +13,7 @@ pkg = {
                      "info.classification" : "Web Services"  },
     "dirtrees" : [ "glassfish" ],
     "depends" : { 
-                  "pkg:/jersey@1.1.1" : {"type" : "require" }
+                  "pkg:/jersey@1.1.2" : {"type" : "require" }
                 },
     "licenses" : { "glassfish/jersey/LICENSE.txt" : { "license" : "CDDL+GPL" }}
 }
