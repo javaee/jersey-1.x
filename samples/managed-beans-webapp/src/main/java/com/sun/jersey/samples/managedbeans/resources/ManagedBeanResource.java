3d132267a47fd0a6c922c86fab01163f7d676bdd
Pavel Bucek <pavel.bucek@oracle.com>
2009-09-14 12:41:57 +0000

    initial version of managed beans sample

---
+++ samples/managed-beans-webapp/src/main/java/com/sun/jersey/samples/managedbeans/resources/ManagedBeanResource.java
@@ -0,0 +1,41 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007 Sun Microsystems, Inc. All rights reserved. 
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License("CDDL") (the "License").  You may not use this file
+ * except in compliance with the License. 
+ * 
+ * You can obtain a copy of the License at:
+ *     https://jersey.dev.java.net/license.txt
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice in each
+ * file and include the License file at:
+ *     https://jersey.dev.java.net/license.txt
+ * If applicable, add the following below this CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+package com.sun.jersey.samples.managedbeans.resources;
+
+import javax.annotation.Resource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+// The Java class will be hosted at the URI path "/managedbean"
+@Path("/managedbean")
+public class ManagedBeanResource {
+
+    @Resource int injectedResource = 50;
+
+    @GET 
+    @Produces("text/plain")
+    public String getMessage() {
+        return "Injected resource value: " + Integer.toString(injectedResource);
+    }
+}
