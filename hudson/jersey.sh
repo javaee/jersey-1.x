34f0c1b16d308adb01c111052110ec507c2d8687
Jakub Podlesak <jakub.podlesak@oracle.com>
2009-09-29 08:28:16 +0000

    moving jersey.sh script to jersey/hudson, so that the whole jersey dirtree does not need to be downloaded everytime hudson builds jersey

---
+++ hudson/jersey.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+cd jersey
+# beta-testing Maven 2.0.9 on Hudson slaves --- this will become the default Maven version soon (Kohsuke)
+export M2_HOME=/files/hudson/tools/maven-2.0.9
+export PATH=$M2_HOME/bin:$PATH
+export MAVEN_OPTS="-Xmx1024m"
+mvn -version
+java -version
+
+# mvn dependency:resolve
+# clean install will test as well
+JERSEY_HTTP_SLEEP=2000 JERSEY_HTTP_STOPSEC=2 mvn -e clean install
+# jersey-bundle needs to be tested as well
+# JERSEY_HTTP_SLEEP=2000 JERSEY_HTTP_STOPSEC=2 mvn -e clean test -Pbundle-dependency
+# 3 attempts to deploy:
+mvn -Dmaven.test.skip=true deploy || mvn -Dmaven.test.skip=true deploy || mvn -Dmaven.test.skip=true deploy 
