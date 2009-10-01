c64a3e10cb826af0889b3e9481401ab1877645f6
Paul Sandoz <paul.sandoz@oracle.com>
2009-10-01 12:53:19 +0000

    Move class into another package so it does not interfere with other tests.

--- jersey-tests/src/test/java/com/sun/jersey/impl/container/ResourceConfiguratorTest.java
+++ jersey-tests/src/test/java/com/sun/jersey/impl/container/ResourceConfiguratorTest.java
@@ -34,7 +34,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.jersey.impl.container.config;
+package com.sun.jersey.impl.container;
 
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.core.DefaultResourceConfig;
