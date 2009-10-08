6fa16cbfcebc86e6bb24ffe8835dfef575335183
Jakub Podlesak <jakub.podlesak@oracle.com>
2009-10-08 09:55:29 +0000

    adding ejb application archetype

---
+++ archetypes/jersey-quickstart-ejb/src/main/resources/archetype-resources/src/main/java/MyResource.java
@@ -0,0 +1,24 @@
+
+package $package;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/** Example resource class hosted at the URI path "/myresource"
+ */
+@Path("/myresource")
+@Stateless
+public class MyResource {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String getIt() {
+        return "Hi there!";
+    }
+}
