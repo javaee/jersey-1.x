756552faba64ccc4501cfbc39cc96864169ce647
Jakub Podlesak <jakub.podlesak@oracle.com>
2010-01-26 16:32:42 +0000

    osgi modules: need at least java version 1.6, minor tweaks in jersey-server/jersey-client module dependency configuration

--- osgi/jersey-core/src/main/java/org/apache/servicemix/specs/locator/OsgiLocator.java
+++ osgi/jersey-core/src/main/java/org/apache/servicemix/specs/locator/OsgiLocator.java
@@ -23,8 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
 
 public class OsgiLocator {
 
