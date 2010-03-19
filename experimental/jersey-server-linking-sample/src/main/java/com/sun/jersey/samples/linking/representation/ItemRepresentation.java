745c4776066b0a73f83b0bee0460fea4cec3e70a
Marc Hadley <marc.hadley@sun.com>
2010-03-19 22:05:19 +0000

    New sample to demonstrate declarative hyperlinking annotations. Simple list traversal with contextual next and prev links both in the response entity and as HTTP link headers.

---
+++ experimental/jersey-server-linking-sample/src/main/java/com/sun/jersey/samples/linking/representation/ItemRepresentation.java
@@ -0,0 +1,125 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://jersey.dev.java.net/CDDL+GPL.html
+ * or jersey/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at jersey/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.jersey.samples.linking.representation;
+
+import com.sun.jersey.samples.linking.resources.ItemResource;
+import com.sun.jersey.server.linking.Binding;
+import com.sun.jersey.server.linking.Link;
+import com.sun.jersey.server.linking.Links;
+import com.sun.jersey.server.linking.Ref;
+import com.sun.jersey.server.linking.Ref.Style;
+import java.net.URI;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mh124079
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name="item")
+@Links({
+    @Link(
+        value=@Ref(
+            resource=ItemResource.class,
+            style = Style.ABSOLUTE,
+            condition="${resource.next}",
+            bindings=@Binding(name="index", value="${entity.index+1}")
+        ),
+        rel="next"
+    ),
+    @Link(
+        value=@Ref(
+            resource=ItemResource.class,
+            style = Style.ABSOLUTE,
+            condition="${resource.prev}",
+            bindings=@Binding(name="index", value="${entity.index-1}")
+        ),
+        rel="prev"
+    )
+})
+public class ItemRepresentation {
+
+    @XmlElement
+    String name;
+
+    int index;
+
+    @Ref(
+        resource=ItemResource.class,
+        style = Style.ABSOLUTE,
+        condition="${resource.next}"
+    )
+    @XmlElement
+    URI self;
+
+    @Ref(
+        resource=ItemResource.class,
+        style = Style.ABSOLUTE,
+        condition="${resource.next}",
+        bindings=@Binding(name="index", value="${entity.index+1}")
+    )
+    @XmlElement
+    URI next;
+
+    @Ref(
+        resource=ItemResource.class,
+        style = Style.ABSOLUTE,
+        condition="${resource.prev}",
+        bindings=@Binding(name="index", value="${entity.index-1}")
+    )
+    @XmlElement
+    URI prev;
+
+    public ItemRepresentation() {
+        this.index = 0;
+        this.name = "";
+    }
+
+    public ItemRepresentation(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+}
