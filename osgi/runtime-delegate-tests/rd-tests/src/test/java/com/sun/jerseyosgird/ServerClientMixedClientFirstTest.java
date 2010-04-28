42eaa747c3990ec9b0710d616facca48ca2a58a6
Jakub Podlesak <jakub.podlesak@oracle.com>
2010-04-28 13:10:38 +0000

    updating version number also at the runtime delegate osgi tests

--- osgi/runtime-delegate-tests/rd-tests/src/test/java/com/sun/jerseyosgird/ServerClientMixedClientFirstTest.java
+++ osgi/runtime-delegate-tests/rd-tests/src/test/java/com/sun/jerseyosgird/ServerClientMixedClientFirstTest.java
@@ -177,10 +177,10 @@ public class ServerClientMixedClientFirstTest {
                 mavenBundle("org.ops4j.pax.url","pax-url-war","1.1.2"),
                 
                 mavenBundle("javax.ws.rs","jsr311-api","1.1.1"),
-                mavenBundle("com.sun.jersey","jersey-core","1.2-SNAPSHOT"),
-        	mavenBundle("com.sun.jersey","jersey-server", "1.2-SNAPSHOT"),
-        	mavenBundle("com.sun.jersey","jersey-client", "1.2-SNAPSHOT"),
-        	mavenBundle("com.sun.jersey.test.osgi.runtime-delegate-tests","runtime-delegate-test-bundle", "1.2-SNAPSHOT"),
+                mavenBundle("com.sun.jersey","jersey-core","1.3-SNAPSHOT"),
+        	mavenBundle("com.sun.jersey","jersey-server", "1.3-SNAPSHOT"),
+        	mavenBundle("com.sun.jersey","jersey-client", "1.3-SNAPSHOT"),
+        	mavenBundle("com.sun.jersey.test.osgi.runtime-delegate-tests","runtime-delegate-test-bundle", "1.3-SNAPSHOT"),
 
                 mavenBundle("com.sun.grizzly", "grizzly-servlet-webserver", "1.9.18-i"),
 
