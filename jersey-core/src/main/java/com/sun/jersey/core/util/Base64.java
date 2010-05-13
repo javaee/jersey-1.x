7fa4a311b45c5535599675c101e0bc937e92a6ad
Roberto Chinnici <roberto.chinnici@oracle.com>
2010-05-13 16:51:14 +0000

    - Fixed issue 510   https://jersey.dev.java.net/issues/show_bug.cgi?id=510   Jersey contains arguably defective Base64 implementation

--- jersey-core/src/main/java/com/sun/jersey/core/util/Base64.java
+++ jersey-core/src/main/java/com/sun/jersey/core/util/Base64.java
@@ -54,84 +54,252 @@
 package com.sun.jersey.core.util;
 
 /**
- * This class provides encode/decode for RFC 2045 Base64 as
- * defined by RFC 2045, N. Freed and N. Borenstein.
- * RFC 2045: Multipurpose Internet Mail Extensions (MIME)
- * Part One: Format of Internet Message Bodies. Reference
- * 1996 Available at: http://www.ietf.org/rfc/rfc2045.txt
- * This class is used by XML Schema binary format validation
+ * This class provides encode/decode for RFC 2045 Base64 as defined by
+ * RFC 2045, N. Freed and N. Borenstein.  <a
+ * href="http://www.ietf.org/rfc/rfc2045.txt">RFC 2045</a>:
+ * Multipurpose Internet Mail Extensions (MIME) Part One: Format of
+ * Internet Message Bodies. Reference 1996
  *
  * @author Jeffrey Rodriguez
- * @version $Revision: 1.3 $ $Date: 2006/03/12 01:27:13 $
  */
-public class Base64 {
+public final class  Base64
+{
+    static private final int  BASELENGTH         = 255;
+    static private final int  LOOKUPLENGTH       = 64;
+    static private final int  TWENTYFOURBITGROUP = 24;
+    static private final int  EIGHTBIT           = 8;
+    static private final int  SIXTEENBIT         = 16;
+    static private final int  SIXBIT             = 6;
+    static private final int  FOURBYTE           = 4;
+    static private final int  SIGN               = -128;
+    static private final byte PAD                = (byte) '=';
+    static private byte [] base64Alphabet       = new byte[BASELENGTH];
+    static private byte [] lookUpBase64Alphabet = new byte[LOOKUPLENGTH];
 
-    static private final int BASELENGTH = 255;
-    static private final int LOOKUPLENGTH = 63;
-    static private final int TWENTYFOURBITGROUP = 24;
-    static private final int EIGHTBIT = 8;
-    static private final int SIXTEENBIT = 16;
-    static private final int SIXBIT = 6;
-    static private final int FOURBYTE = 4;
-    static private final byte PAD = (byte) '=';
-    static private byte[] base64Alphabet = new byte[BASELENGTH];
-    static private byte[] lookUpBase64Alphabet = new byte[LOOKUPLENGTH];
-
-
-    static {
-
-        for (int i = 0; i < BASELENGTH; i++) {
+    static
+    {
+        for (int i = 0; i < BASELENGTH; i++ )
+        {
             base64Alphabet[i] = -1;
         }
-        for (int i = 'Z'; i >= 'A'; i--) {
+        for (int i = 'Z'; i >= 'A'; i--)
+        {
             base64Alphabet[i] = (byte) (i - 'A');
         }
-        for (int i = 'z'; i >= 'a'; i--) {
+        for (int i = 'z'; i>= 'a'; i--)
+        {
             base64Alphabet[i] = (byte) (i - 'a' + 26);
         }
-
-        for (int i = '9'; i >= '0'; i--) {
+        for (int i = '9'; i >= '0'; i--)
+        {
             base64Alphabet[i] = (byte) (i - '0' + 52);
         }
 
-        base64Alphabet['+'] = 62;
-        base64Alphabet['/'] = 63;
+        base64Alphabet['+']  = 62;
+        base64Alphabet['/']  = 63;
 
-        for (int i = 0; i <= 25; i++) {
+        for (int i = 0; i <= 25; i++ )
             lookUpBase64Alphabet[i] = (byte) ('A' + i);
-        }
 
-        for (int i = 26, j = 0; i <= 51; i++, j++) {
-            lookUpBase64Alphabet[i] = (byte) ('a' + j);
-        }
+        for (int i = 26,  j = 0; i <= 51; i++, j++ )
+            lookUpBase64Alphabet[i] = (byte) ('a'+ j);
 
-        for (int i = 52, j = 0; i <= 61; i++, j++) {
+        for (int i = 52,  j = 0; i <= 61; i++, j++ )
             lookUpBase64Alphabet[i] = (byte) ('0' + j);
-        }
 
+        lookUpBase64Alphabet[62] = (byte) '+';
+        lookUpBase64Alphabet[63] = (byte) '/';
     }
 
-    static boolean isBase64(byte octect) {
+    public static boolean isBase64( String isValidString )
+    {
+        return isArrayByteBase64(isValidString.getBytes());
+    }
+
+    public static boolean isBase64( byte octect )
+    {
         //shall we ignore white space? JEFF??
         return (octect == PAD || base64Alphabet[octect] != -1);
     }
 
-    static boolean isArrayByteBase64(byte[] arrayOctect) {
+    public static boolean isArrayByteBase64( byte[] arrayOctect )
+    {
         int length = arrayOctect.length;
-        if (length == 0) {
-            return false;
+        if (length == 0)
+        {
+            // shouldn't a 0 length array be valid base64 data?
+            // return false;
+            return true;
         }
-        for (int i = 0; i < length; i++) {
-            if (Base64.isBase64(arrayOctect[i]) == false) {
+        for (int i=0; i < length; i++)
+        {
+            if ( !Base64.isBase64(arrayOctect[i]) )
                 return false;
-            }
         }
         return true;
     }
 
+    /**
+     * Encodes hex octects into Base64.
+     *
+     * @param binaryData Array containing binary data to encode.
+     * @return Base64-encoded data.
+     */
+    public static byte[] encode( byte[] binaryData )
+    {
+        int      lengthDataBits    = binaryData.length*EIGHTBIT;
+        int      fewerThan24bits   = lengthDataBits%TWENTYFOURBITGROUP;
+        int      numberTriplets    = lengthDataBits/TWENTYFOURBITGROUP;
+        byte     encodedData[]     = null;
+
+
+        if (fewerThan24bits != 0)
+        {
+            //data not divisible by 24 bit
+            encodedData = new byte[ (numberTriplets + 1 ) * 4 ];
+        }
+        else
+        {
+            // 16 or 8 bit
+            encodedData = new byte[ numberTriplets * 4 ];
+        }
+
+        byte k = 0, l = 0, b1 = 0, b2 = 0, b3 = 0;
+
+        int encodedIndex = 0;
+        int dataIndex   = 0;
+        int i           = 0;
+        for ( i = 0; i<numberTriplets; i++ )
+        {
+            dataIndex = i*3;
+            b1 = binaryData[dataIndex];
+            b2 = binaryData[dataIndex + 1];
+            b3 = binaryData[dataIndex + 2];
+
+            l  = (byte)(b2 & 0x0f);
+            k  = (byte)(b1 & 0x03);
+
+            encodedIndex = i * 4;
+            byte val1 = ((b1 & SIGN)==0)?(byte)(b1>>2):(byte)((b1)>>2^0xc0);
+            byte val2 = ((b2 & SIGN)==0)?(byte)(b2>>4):(byte)((b2)>>4^0xf0);
+            byte val3 = ((b3 & SIGN)==0)?(byte)(b3>>6):(byte)((b3)>>6^0xfc);
+
+            encodedData[encodedIndex]   = lookUpBase64Alphabet[ val1 ];
+            encodedData[encodedIndex+1] =
+                lookUpBase64Alphabet[ val2 | ( k<<4 )];
+            encodedData[encodedIndex+2] =
+                lookUpBase64Alphabet[ (l <<2 ) | val3 ];
+            encodedData[encodedIndex+3] = lookUpBase64Alphabet[ b3 & 0x3f ];
+        }
+
+        // form integral number of 6-bit groups
+        dataIndex    = i*3;
+        encodedIndex = i*4;
+        if (fewerThan24bits == EIGHTBIT )
+        {
+            b1 = binaryData[dataIndex];
+            k = (byte) ( b1 &0x03 );
+            byte val1 = ((b1 & SIGN)==0)?(byte)(b1>>2):(byte)((b1)>>2^0xc0);
+            encodedData[encodedIndex]     = lookUpBase64Alphabet[ val1 ];
+            encodedData[encodedIndex + 1] = lookUpBase64Alphabet[ k<<4 ];
+            encodedData[encodedIndex + 2] = PAD;
+            encodedData[encodedIndex + 3] = PAD;
+        }
+        else if (fewerThan24bits == SIXTEENBIT)
+        {
+
+            b1 = binaryData[dataIndex];
+            b2 = binaryData[dataIndex +1 ];
+            l = (byte) (b2 & 0x0f);
+            k = (byte) (b1 & 0x03);
+
+            byte val1 = ((b1 & SIGN) == 0)?(byte)(b1>>2):(byte)((b1)>>2^0xc0);
+            byte val2 = ((b2 & SIGN) == 0)?(byte)(b2>>4):(byte)((b2)>>4^0xf0);
+
+            encodedData[encodedIndex]     = lookUpBase64Alphabet[ val1 ];
+            encodedData[encodedIndex + 1] =
+                lookUpBase64Alphabet[ val2 | ( k<<4 )];
+            encodedData[encodedIndex + 2] = lookUpBase64Alphabet[ l<<2 ];
+            encodedData[encodedIndex + 3] = PAD;
+        }
+
+        return encodedData;
+    }
 
     /**
-     * Encodes hex octects of a UTF-8 encoded String into Base64
+     * Decodes Base64 data into octects
+     *
+     * @param binaryData Byte array containing Base64 data
+     * @return Array containing decoded data.
+     */
+    public static byte[] decode( byte[] base64Data )
+    {
+        // handle the edge case, so we don't have to worry about it later
+        if(base64Data.length == 0) { return new byte[0]; }
+
+        int      numberQuadruple    = base64Data.length/FOURBYTE;
+        byte     decodedData[]      = null;
+        byte     b1=0,b2=0,b3=0, b4=0, marker0=0, marker1=0;
+
+        // Throw away anything not in base64Data
+
+        int encodedIndex = 0;
+        int dataIndex    = 0;
+        {
+            // this sizes the output array properly - rlw
+            int lastData = base64Data.length;
+            // ignore the '=' padding
+            while (base64Data[lastData-1] == PAD)
+            {
+                if (--lastData == 0)
+                {
+                    return new byte[0];
+                }
+            }
+            decodedData = new byte[ lastData - numberQuadruple ];
+        }
+
+        for (int i = 0; i < numberQuadruple; i++)
+        {
+            dataIndex = i * 4;
+            marker0   = base64Data[dataIndex + 2];
+            marker1   = base64Data[dataIndex + 3];
+
+            b1 = base64Alphabet[base64Data[dataIndex]];
+            b2 = base64Alphabet[base64Data[dataIndex +1]];
+
+            if (marker0 != PAD && marker1 != PAD)
+            {
+                //No PAD e.g 3cQl
+                b3 = base64Alphabet[ marker0 ];
+                b4 = base64Alphabet[ marker1 ];
+
+                decodedData[encodedIndex]   = (byte)(  b1 <<2 | b2>>4 ) ;
+                decodedData[encodedIndex + 1] =
+                    (byte)(((b2 & 0xf)<<4 ) |( (b3>>2) & 0xf) );
+                decodedData[encodedIndex + 2] = (byte)( b3<<6 | b4 );
+            }
+            else if (marker0 == PAD)
+            {
+                //Two PAD e.g. 3c[Pad][Pad]
+                decodedData[encodedIndex]   = (byte)(  b1 <<2 | b2>>4 ) ;
+            }
+            else if (marker1 == PAD)
+            {
+                //One PAD e.g. 3cQ[Pad]
+                b3 = base64Alphabet[ marker0 ];
+
+                decodedData[encodedIndex]   = (byte)(  b1 <<2 | b2>>4 );
+                decodedData[encodedIndex + 1] =
+                    (byte)(((b2 & 0xf)<<4 ) |( (b3>>2) & 0xf) );
+            }
+            encodedIndex += 3;
+        }
+        return decodedData;
+    }
+     
+    /**
+     * Encodes hex octets of a UTF-8 encoded String into Base64
      *
      * @param data the String to encode.
      * @return Encoded Base64 array
@@ -139,125 +307,17 @@ public class Base64 {
     public static byte[] encode(String data) {
         return encode(data.getBytes());
     }
-    
-    /**
-     * Encodes hex octects into Base64
-     *
-     * @param binaryData Array containing binaryData
-     * @return Encoded Base64 array
-     */
-    public static byte[] encode(byte[] binaryData) {
-        int lengthDataBits = binaryData.length * EIGHTBIT;
-        int fewerThan24bits = lengthDataBits % TWENTYFOURBITGROUP;
-        int numberTriplets = lengthDataBits / TWENTYFOURBITGROUP;
-        byte encodedData[] = null;
-
-
-        if (fewerThan24bits != 0) //data not divisible by 24 bit
-        {
-            encodedData = new byte[(numberTriplets + 1) * 4];
-        } else // 16 or 8 bit
-        {
-            encodedData = new byte[numberTriplets * 4];
-        }
-
-        byte k = 0, l = 0, b1 = 0, b2 = 0, b3 = 0;
-
-        int encodedIndex = 0;
-        int dataIndex = 0;
-        int i = 0;
-        for (i = 0; i < numberTriplets; i++) {
-
-            dataIndex = i * 3;
-            b1 = binaryData[dataIndex];
-            b2 = binaryData[dataIndex + 1];
-            b3 = binaryData[dataIndex + 2];
-
-            l = (byte) (b2 & 0x0f);
-            k = (byte) (b1 & 0x03);
-
-            encodedIndex = i * 4;
-            encodedData[encodedIndex] = lookUpBase64Alphabet[b1 >> 2];
-            encodedData[encodedIndex + 1] = lookUpBase64Alphabet[(b2 >> 4) |
-                    (k << 4)];
-            encodedData[encodedIndex + 2] = lookUpBase64Alphabet[(l << 2) |
-                    (b3 >> 6)];
-            encodedData[encodedIndex + 3] = lookUpBase64Alphabet[b3 & 0x3f];
-        }
-
-        // form integral number of 6-bit groups
-        dataIndex = i * 3;
-        encodedIndex = i * 4;
-        if (fewerThan24bits == EIGHTBIT) {
-            b1 = binaryData[dataIndex];
-            k = (byte) (b1 & 0x03);
-            encodedData[encodedIndex] = lookUpBase64Alphabet[b1 >> 2];
-            encodedData[encodedIndex + 1] = lookUpBase64Alphabet[k << 4];
-            encodedData[encodedIndex + 2] = PAD;
-            encodedData[encodedIndex + 3] = PAD;
-        } else if (fewerThan24bits == SIXTEENBIT) {
-
-            b1 = binaryData[dataIndex];
-            b2 = binaryData[dataIndex + 1];
-            l = (byte) (b2 & 0x0f);
-            k = (byte) (b1 & 0x03);
-            encodedData[encodedIndex] = lookUpBase64Alphabet[b1 >> 2];
-            encodedData[encodedIndex + 1] = lookUpBase64Alphabet[(b2 >> 4) | (k << 4)];
-            encodedData[encodedIndex + 2] = lookUpBase64Alphabet[l << 2];
-            encodedData[encodedIndex + 3] = PAD;
-        }
-        return encodedData;
-    }
 
     /**
-     * Decodes Base64 data into octects
+     * Decodes a string containing Base64 data
      *
-     * @param base64Data Byte array containing Base64 data
-     * @return Array containind decoded data.
+     * @param data the String to decode.
+     * @return Decoded Base64 array
      */
-    public static byte[] decode(byte[] base64Data) {
-        int numberQuadruple = base64Data.length / FOURBYTE;
-        byte decodedData[] = null;
-        byte b1 = 0, b2 = 0, b3 = 0, b4 = 0, marker0 = 0, marker1 = 0;
-
-        // Throw away anything not in base64Data
-        // Adjust size
-
-        int encodedIndex = 0;
-        int dataIndex = 0;
-        decodedData = new byte[numberQuadruple * 3 + 1];
-
-        for (int i = 0; i < numberQuadruple; i++) {
-            dataIndex = i * 4;
-            marker0 = base64Data[dataIndex + 2];
-            marker1 = base64Data[dataIndex + 3];
-
-            b1 = base64Alphabet[base64Data[dataIndex]];
-            b2 = base64Alphabet[base64Data[dataIndex + 1]];
-
-            if (marker0 != PAD && marker1 != PAD) {     //No PAD e.g 3cQl
-                b3 = base64Alphabet[marker0];
-                b4 = base64Alphabet[marker1];
-
-                decodedData[encodedIndex] = (byte) (b1 << 2 | b2 >> 4);
-                decodedData[encodedIndex + 1] = (byte) (((b2 & 0xf) << 4) | ((b3 >> 2) & 0xf));
-                decodedData[encodedIndex + 2] = (byte) (b3 << 6 | b4);
-            } else if (marker0 == PAD) {               //Two PAD e.g. 3c[Pad][Pad]
-                decodedData[encodedIndex] = (byte) (b1 << 2 | b2 >> 4);
-                decodedData[encodedIndex + 1] = (byte) ((b2 & 0xf) << 4);
-                decodedData[encodedIndex + 2] = (byte) 0;
-            } else if (marker1 == PAD) {              //One PAD e.g. 3cQ[Pad]
-                b3 = base64Alphabet[marker0];
-
-                decodedData[encodedIndex] = (byte) (b1 << 2 | b2 >> 4);
-                decodedData[encodedIndex + 1] = (byte) (((b2 & 0xf) << 4) | ((b3 >> 2) & 0xf));
-                decodedData[encodedIndex + 2] = (byte) (b3 << 6);
-            }
-            encodedIndex += 3;
-        }
-        return decodedData;
-
+    public static byte[] decode(String data) {
+        return decode(data.getBytes());
     }
+          
     static final int base64[] = {
         64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
         64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
---
+++ jersey-core/src/test/java/com/sun/jersey/impl/util/Base64Test.java
@@ -0,0 +1,99 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://jersey.dev.java.net/CDDL+GPL.html
+ * or jersey/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at jersey/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+ 
+package com.sun.jersey.impl.util;
+
+import junit.framework.TestCase;
+import static junit.framework.Assert.assertTrue;
+import com.sun.jersey.core.util.Base64;
+import java.util.Arrays;
+
+/**
+ *
+ * @author robc
+ */
+public class Base64Test extends TestCase {
+    
+    public void testRoundtripLengthMod3Equals0() {
+        byte[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        byte[] result = Base64.decode(Base64.encode(data));
+        assertTrue("failed to roundtrip value to base64", Arrays.equals(data, result));
+    }
+
+    public void testRoundtripLengthMod3Equals1() {
+        byte[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        byte[] result = Base64.decode(Base64.encode(data));
+        assertTrue("failed to roundtrip value to base64", Arrays.equals(data, result));
+    }
+
+    public void testRoundtripLengthMod3Equals2() {
+        byte[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        byte[] result = Base64.decode(Base64.encode(data));
+        assertTrue("failed to roundtrip value to base64", Arrays.equals(data, result));
+    }
+
+    public void testRoundtripOneByteGreaterThan127() {
+        byte[] data = {(byte)128};
+        byte[] result = Base64.decode(Base64.encode(data));
+        assertTrue("failed to roundtrip value to base64", Arrays.equals(data, result));
+    }
+
+    public void testRoundtripAssortedValues() {
+        byte[] data = {0, 1, 63, 64, 65, (byte)127, (byte)128, (byte)1299, (byte)254, (byte)255};
+        byte[] result = Base64.decode(Base64.encode(data));
+        assertTrue("failed to roundtrip value to base64", Arrays.equals(data, result));
+    }
+
+    public void testEncodeByteArray() {
+        String expectedResult = "//79/Pv6+fj39vX08/Lx8O/u7ezr6uno5+bl5OPi4eDf3t3c29rZ2NfW1dTT0tHQz87NzMvKycjHxsXEw8LBwL++vby7urm4t7a1tLOysbCvrq2sq6qpqKempaSjoqGgn56dnJuamZiXlpWUk5KRkI+OjYyLiomIh4aFhIOCgYB/fn18e3p5eHd2dXRzcnFwb25tbGtqaWhnZmVkY2JhYF9eXVxbWllYV1ZVVFNSUVBPTk1MS0pJSEdGRURDQkFAPz49PDs6OTg3NjU0MzIxMC8uLSwrKikoJyYlJCMiISAfHh0cGxoZGBcWFRQTEhEQDw4NDAsKCQgHBgUEAwIBAA==";
+        byte[] data = new byte[256];
+        for (int i = 0; i < 256; ++i) {
+            data[i] = (byte)(255 - i);
+        }
+        String result = new String(Base64.encode(data));
+        assertEquals("incorrect value", result, expectedResult);
+    }
+    
+    public void testDecodeString() {
+        String data = "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w==";
+        byte[] result = Base64.decode(data);
+        assertEquals("incorrect length", result.length, 256);
+        for (int i = 0; i < 256; ++i) {
+            assertEquals("incorrect value", result[i], (byte)i);
+        }
+    }
+}
