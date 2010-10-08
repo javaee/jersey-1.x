0daf2cabed8c0f412dc0cb295e8e07e3d68e5642
Pavel Bucek <pavel.bucek@oracle.com>
2010-10-08 16:53:12 +0000

    1.5-ea01

--- glassfish/v3-packages/core/src/main/resources/jersey-core_proto.py
+++ glassfish/v3-packages/core/src/main/resources/jersey-core_proto.py
@@ -1,6 +1,6 @@
 pkg = {
     "name"          : "jersey",
-    "version"       : "1.5,0-0.1",
+    "version"       : "1.5,0-0.3",
     "attributes"    : { "pkg.summary" : "Jersey Core, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "Jersey core runtime libraries including some 3rd party dependencies. \
