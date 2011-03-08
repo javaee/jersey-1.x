806255e770bc4a5cf03565175bfc029262f8b098
Jakub Podlesak <jakub.podlesak@oracle.com>
2011-03-08 12:36:17 +0000

    fixing broken commit

--- samples/jacksonjsonprovider/src/test/java/com/sun/jersey/samples/jacksonjsonprovider/MainTest.java
+++ samples/jacksonjsonprovider/src/test/java/com/sun/jersey/samples/jacksonjsonprovider/MainTest.java
@@ -62,8 +62,7 @@ public class MainTest extends JerseyTest {
     public void testEmptyArrayPresent() {
         WebResource r = resource();
         String responseMsg = r.path("emptyArrayResource").accept(MediaType.APPLICATION_JSON).get(String.class);
-        responseMsg.replaceAll("[ \t]", "");
-        assertTrue(responseMsg.contains("[]"));
+        assertTrue(responseMsg.replaceAll("[ \t]*", "").contains("[]"));
     }
 
     @Test
