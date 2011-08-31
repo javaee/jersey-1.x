bb3dc89d5c1f1ca00447aae0850f56f940714612
Martin Matula <martin.matula@oracle.com>
2011-08-31 16:14:03 +0000

    CSRF protection filters added to jersey-client and jersey-server

---
+++ jersey-tests/src/test/java/com/sun/jersey/impl/client/filter/CsrfProtectionFilterTest.java
@@ -0,0 +1,143 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.jersey.impl.client.filter;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.CsrfProtectionFilter;
+import com.sun.jersey.impl.container.grizzly.AbstractGrizzlyServerTester;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.HttpHeaders;
+
+/**
+ * @author Martin Matula
+ */
+public class CsrfProtectionFilterTest extends AbstractGrizzlyServerTester {
+
+    private static final String HEADER_NAME = "X-Requested-By";
+
+    @Path("/resource")
+    public static class Resource {
+
+        @Context
+        HttpHeaders h;
+
+        @GET
+        public String get() {
+            return toString(h.getRequestHeader(HEADER_NAME));
+        }
+
+        @PUT
+        public String put() {
+            return toString(h.getRequestHeader(HEADER_NAME));
+        }
+
+        private static String toString(Object o) {
+            return (o == null) ? null : o.toString();
+        }
+    }
+
+    public CsrfProtectionFilterTest(String testName) {
+        super(testName);
+    }
+
+    public void testEmptyConstGet() {
+        startServer(Resource.class);
+
+        Client c = Client.create();
+        c.addFilter(new CsrfProtectionFilter());
+        WebResource r = c.resource(getUri().path("resource").build());
+
+        ClientResponse response = r.get(ClientResponse.class);
+        assertEquals("Status code did not match.", 204, response.getStatus());
+    }
+
+    public void testEmptyConstPut() {
+        startServer(Resource.class);
+
+        Client c = Client.create();
+        c.addFilter(new CsrfProtectionFilter());
+        WebResource r = c.resource(getUri().path("resource").build());
+
+        ClientResponse response = r.put(ClientResponse.class);
+        assertEquals("Status code did not match.", 200, response.getStatus());
+        assertEquals("Value did not match.", "[]", response.getEntity(String.class));
+    }
+
+    public void testNonEmptyConstPut() {
+        startServer(Resource.class);
+
+        Client c = Client.create();
+        c.addFilter(new CsrfProtectionFilter("test"));
+        WebResource r = c.resource(getUri().path("resource").build());
+
+        ClientResponse response = r.put(ClientResponse.class);
+        assertEquals("Status code did not match.", 200, response.getStatus());
+        assertEquals("Value did not match.", "[test]", response.getEntity(String.class));
+    }
+
+    public void testOverridePut() {
+        startServer(Resource.class);
+
+        Client c = Client.create();
+        c.addFilter(new CsrfProtectionFilter("test"));
+        WebResource r = c.resource(getUri().path("resource").build());
+
+        ClientResponse response = r.header(HEADER_NAME, "testOverridePut").put(ClientResponse.class);
+        assertEquals("Status code did not match.", 200, response.getStatus());
+        assertEquals("Value did not match.", "[testOverridePut]", response.getEntity(String.class));
+    }
+
+    public void testOverrideGet() {
+        startServer(Resource.class);
+
+        Client c = Client.create();
+        c.addFilter(new CsrfProtectionFilter("test"));
+        WebResource r = c.resource(getUri().path("resource").build());
+
+        ClientResponse response = r.header(HEADER_NAME, "testOverrideGet").get(ClientResponse.class);
+        assertEquals("Status code did not match.", 200, response.getStatus());
+        assertEquals("Value did not match.", "[testOverrideGet]", response.getEntity(String.class));
+    }
+}
