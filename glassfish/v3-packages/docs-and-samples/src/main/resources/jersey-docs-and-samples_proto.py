d6f71937019d0a50c076984eedadf94d9d4ac9ae
Pavel Bucek <pavel.bucek@oracle.com>
2011-11-15 09:36:55 +0000

    1.11-b01

--- glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
+++ glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
@@ -1,6 +1,6 @@
 pkg = {
     "name"          : "jersey-docs-and-examples",
-    "version"       : "1.11,0-0.1",
+    "version"       : "1.11,0-0.3",
     "attributes"    : { "pkg.summary" : "Jersey Examples And Documentation, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "This package contains Jersey API javadocs and examples. Once installed, \
@@ -13,7 +13,7 @@ pkg = {
                      "info.classification" : "Web Services"  },
     "dirtrees" : [ "glassfish" ],
     "depends" : { 
-                  "pkg:/jersey@1.11,0-0.1" : {"type" : "require" }
+                  "pkg:/jersey@1.11,0-0.3" : {"type" : "require" }
                 },
     "licenses" : { "glassfish/jersey/LICENSE.txt" : { "license" : "CDDL+GPL" }}
 }
