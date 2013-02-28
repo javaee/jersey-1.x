18207f23bd120f94ca3f2273812d99810f6ed362
michalgajdos <michalgajdos@2fda5173-fe30-0410-90bd-98ff5453417f>
2013-02-28 11:27:52 +0000

    1.17 -> 1.17.1 in properties
    git-svn-id: https://svn.java.net/svn/jersey~svn/branches/jersey-1.17.1/jersey@5838 2fda5173-fe30-0410-90bd-98ff5453417f
    Change-Id: I7e3bf5c79bc532370020f237f920348f862cf7aa

--- glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
+++ glassfish/v3-packages/docs-and-samples/src/main/resources/jersey-docs-and-samples_proto.py
@@ -1,6 +1,6 @@
 pkg = {
     "name"          : "jersey-docs-and-examples",
-    "version"       : "1.17,0-1.2",
+    "version"       : "1.17.1,0-1.2",
     "attributes"    : { "pkg.summary" : "Jersey Examples And Documentation, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "This package contains Jersey API javadocs and examples. Once installed, \
@@ -13,7 +13,7 @@ pkg = {
                      "info.classification" : "Web Services"  },
     "dirtrees" : [ "glassfish" ],
     "depends" : { 
-                  "pkg:/jersey@1.17,0-1.2" : {"type" : "require" }
+                  "pkg:/jersey@1.17.1,0-1.2" : {"type" : "require" }
                 },
     "licenses" : { "glassfish/jersey/LICENSE.txt" : { "license" : "CDDL+GPL" }}
 }
--- glassfish/v3.2-packages/core/src/main/resources/jersey-core_proto.py
+++ glassfish/v3.2-packages/core/src/main/resources/jersey-core_proto.py
@@ -1,6 +1,6 @@
 pkg = {
     "name"          : "jersey",
-    "version"       : "1.17,0-1.2",
+    "version"       : "1.17.1,0-1.2",
     "attributes"    : { "pkg.summary" : "Jersey Core, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "Jersey core runtime libraries including some 3rd party dependencies. \
