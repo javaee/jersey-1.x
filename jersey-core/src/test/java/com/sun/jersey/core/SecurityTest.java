66c93f37543e02d2fe91f884adee2b9cff158fd8
Michal Gajdos <michal.gajdos.sk@gmail.com>
2014-02-12 18:20:12 +0100

    JERSEY-2384: Migrate remaining Jersey 1.x hudson jobs.
     - changes to make XDK job work (added security permissions to XDK)
    
    Change-Id: Ic34517666be820287c337000bb0d9450a4324c17
    Signed-off-by: Michal Gajdos <michal.gajdos.sk@gmail.com>

--- jersey-core/src/test/java/com/sun/jersey/core/SecurityTest.java
+++ jersey-core/src/test/java/com/sun/jersey/core/SecurityTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,34 +37,22 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-// we do not care about java lib itself
-grant codebase "file:${java.home}/-" {
-  permission java.security.AllPermission;
-};
+package com.sun.jersey.core;
 
-// we do not care about our dependencies
-grant codebase "file:${settings.localRepository}/-" {
-  permission java.security.AllPermission;
-};
+import java.security.AccessControlException;
 
-// this is to be able to set runtime delegate instance in jax-rs from the tests
-// and to run multi-threaded tests
-grant codebase "file:${project.build.directory}/test-classes/-" {
-  permission java.lang.RuntimePermission "accessDeclaredMembers";
-  permission java.lang.reflect.ReflectPermission "suppressAccessChecks";
-  permission java.io.FilePermission "<<ALL FILES>>", "read";
-  permission java.util.PropertyPermission "*", "read";
-};
+import org.junit.Test;
 
-grant codebase "file:${project.build.directory}/classes/-" {
-  permission java.lang.reflect.ReflectPermission "suppressAccessChecks";
-  permission java.lang.RuntimePermission "accessDeclaredMembers";
-  permission java.lang.RuntimePermission "getClassLoader";
-  permission java.util.PropertyPermission "*", "read";
-  permission java.io.FilePermission "<<ALL FILES>>", "read";
-};
+/**
+ * @author Michal Gajdos (michal.gajdos at oracle.com)
+ */
+public class SecurityTest {
 
-// we do not care about xdk lib
-grant codebase "file:${xdk.absolute.path}" {
-  permission java.security.AllPermission;
-};
+    /**
+     * Negative test-case that ensures that operation which is not permitted throws an exception.
+     */
+    @Test(expected = AccessControlException.class)
+    public void testNegativeSecurityPermission() throws Exception {
+        System.setProperty("foo", "bar");
+    }
+}
